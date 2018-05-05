a739cbc4bef38bb7170318cc651a85c90fc787e2
Kalter-M <ibrusentsev@gmail.com>
2018-05-05 06:14:14 +0300

    add books page, book data

--- src/main/java/ru/task/library_spring/GreetingController.java
+++ src/main/java/ru/task/library_spring/GreetingController.java
@@ -5,32 +5,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import ru.task.library_spring.entity.Book;
 import ru.task.library_spring.entity.User;
+import ru.task.library_spring.repos.BookRepo;
 import ru.task.library_spring.repos.UserRepo;
 
 @Controller
 public class GreetingController {
 
     private final UserRepo userRepo;
+    private final BookRepo bookRepo;
 
     @Autowired
-    public GreetingController(UserRepo userRepo) {
+    public GreetingController(UserRepo userRepo, BookRepo bookRepo) {
         this.userRepo = userRepo;
+        this.bookRepo = bookRepo;
     }
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/books")
+    public String books(Model model) {
+        Iterable<Book> books = bookRepo.findAll();
+        model.addAttribute("books", books);
+        return "books";
     }
 
-    @GetMapping()
-    public String main(String name, Model model) {
+    @GetMapping("/users")
+    public String users(Model model) {
         Iterable<User> users = userRepo.findAll();
-
         model.addAttribute("users", users);
-        return "main";
+        return "users";
+    }
+
+    //redirect to users page
+    @GetMapping()
+    public RedirectView redirectWithUsingRedirectView() {
+        return new RedirectView("/users");
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into user values (1, 'admin', '1234');
 insert into user values (2, 'alice', '1234');
+insert into book values ('asdfghjk', 'book1', 'author1', 1);
+insert into book values ('qwertyui', 'book2', 'author2', null);
