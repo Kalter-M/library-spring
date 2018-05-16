03d2a4d706d53b869b200e4ded88b91eb6de0c03
Kalter-M <ibrusentsev@gmail.com>
2018-05-16 22:23:34 +0300

    login styles

--- src/main/java/ru/task/library_spring/config/WebSecurityConfig.java
+++ src/main/java/ru/task/library_spring/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.task.library_spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,8 +19,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-               // .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(
+                        "/lib/bootstrap/**",
+                        "/css/**",
+                        "/img/**",
+                        "/js/**").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -27,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
-        http.formLogin().defaultSuccessUrl("/users", true);
     }
+
 
     @Bean
     @Override
